0b398b60c90d61708cff858c6c0616b480db1d24
nikita_ivochkin <nikita.ivochkin@icloud.com>
2019-09-20 23:06:33 +0300

    refactoring

--- src/main.js
+++ src/main.js
@@ -12,7 +12,8 @@ const container = document.querySelector('.slider-container');
 
 const state = {
     hide: {
-        node: cActiveEls[cActiveEls.length - 1],
+        nodePrev: cActiveEls[cActiveEls.length - 1],
+        nodeNext: cActiveEls[cActiveEls.length - container.childNodes.length - cActiveEls.length + 1],
     },
     previous: {
         node: cActiveEls[0],
@@ -26,26 +27,50 @@ const state = {
         node: cActiveEls[2],
         position: 2,
     },
+    event: 'next',
+};
+
+const getSlidesBasedOnScreen = {
+    parser: (windowWidth, els) => {
+        if (windowWidth >= 1024) {
+            container.append(els.hidePrev, els.previous, els.current, els.next, els.hideNext);
+        } if (windowWidth >= 768 && windowWidth < 1024) {
+            container.append(els.hidePrev, els.previous, els.current, els.hideNext);
+        } if (windowWidth < 768) {
+            container.append(els.hidePrev, els.previous, els.hideNext);
+        }
+    },
+    quantity: (target, containerLength) => {
+        const slides = target.closest('#slider').querySelector('.slider-inner').querySelectorAll('.slider-item');
+        const length = slides.length;
+        console.log(containerLength)
+        if (containerLength >= 5) {
+            return state.next.position === length - 1 ? 0 : state.next.position + 1
+        } if (containerLength === 4) {
+            return state.current.position === length - 1 ? 0 : state.current.position + 1
+        } if (containerLength === 3) {
+            return state.previous.position === length - 1 ? 0 : state.previous.position + 1
+        }
+    }
 };
 
 const getSliedrElements = (windowWidth) => {
-    const hide = state.hide.node.cloneNode(true);
-    const previous = state.previous.node.cloneNode(true);
-    const current = state.current.node.cloneNode(true);
-    const next = state.next.node.cloneNode(true);
 
-    hide.classList.add('hide');
-    previous.classList.add('active');
-    current.classList.add('active');
-    next.classList.add('active');
+    const elements = {
+        hidePrev: state.hide.nodePrev.cloneNode(true),
+        hideNext: state.hide.nodeNext.cloneNode(true),
+        previous: state.previous.node.cloneNode(true),
+        current: state.current.node.cloneNode(true),
+        next: state.next.node.cloneNode(true),
+    };
     
-    if (windowWidth >= 1024) {
-        container.append(hide, previous, current, next);
-    } if (windowWidth >= 768 && windowWidth < 1024) {
-        container.append(hide, previous, current);
-    } if (windowWidth < 768) {
-        container.append(hide, previous);
-    }
+    elements.hidePrev.classList.add('hidePrev');
+    elements.hideNext.classList.add('hideNext');
+    elements.previous.classList.add('active');
+    elements.current.classList.add('active');
+    elements.next.classList.add('active');
+    
+    getSlidesBasedOnScreen.parser(windowWidth, elements);
 };
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -53,6 +78,7 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 watch(state.previous, 'node', () => {
+    
     animate({
         duration: 1000,
         timing: function(timeFraction) {
@@ -60,22 +86,22 @@ watch(state.previous, 'node', () => {
         },
         draw: function(progress) {
             container.style.right = '0%';
-            container.style.transform = `translateX(${progress * 34}%)`
+            state.event === 'next' ? container.style.transform = `translateX(${progress * 34}%)` :
+                container.style.transform = `translateX(-${progress * 34}%)`
           if (progress === 1) {
             container.innerHTML = null;
             getSliedrElements(window.innerWidth);
-            container.style.right = '34%'
-            container.style.transform = -`translateX(${progress * 34}%)`
+            state.event === 'next' ? container.style.right = '34%' : container.style.right = '-34%'
+            state.event === 'next' ? container.style.transform = `translateX(${progress * 34}%)` :
+                container.style.transform = `translateX(-${progress * 34}%)`
           }
         }
       });
-    // container.innerHTML = null;
-    // getSliedrElements(window.innerWidth);
 });
 
 window.addEventListener('resize', () => {
     container.innerHTML = null;
-    getSliedrElements(window.innerWidth);
+    getSliedrElements(window.innerWidth, state.event);
 });
 
 const nextSlide = ({ target }) => {
@@ -89,12 +115,14 @@ const nextSlide = ({ target }) => {
     } if (state.next.position === 0) {
         state.next.position = length;
     }
-    
+
+    state.event = 'next';
     state.previous.position -= 1;
     state.current.position -= 1;
     state.next.position -= 1;
 
-    state.hide.node = slides[state.previous.position === 0 ? length - 1 : state.previous.position - 1];
+    state.hide.nodePrev = slides[state.previous.position === 0 ? length - 1 : state.previous.position - 1];
+    state.hide.nodeNext = slides[getSlidesBasedOnScreen.quantity(target, container.childNodes.length)]
     state.previous.node = slides[state.previous.position];
     state.current.node = slides[state.current.position];
     state.next.node = slides[state.next.position];
@@ -111,11 +139,14 @@ const prevSlide = ({ target }) => {
     } if (state.previous.position === length) {
         state.previous.position = -1;
     }
-    
+
+    state.event = 'prev';
     state.previous.position += 1;
     state.current.position += 1;
     state.next.position += 1;
     
+    state.hide.nodePrev = slides[state.previous.position === 0 ? length - 1 : state.previous.position - 1];
+    state.hide.nodeNext = slides[getSlidesBasedOnScreen.quantity(target, container.childNodes.length)];
     state.previous.node = slides[state.previous.position];
     state.current.node = slides[state.current.position];
     state.next.node = slides[state.next.position];
